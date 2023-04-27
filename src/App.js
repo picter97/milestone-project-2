a858e6081a920f7495034c84f3d5601b6798b588
picter <picter2esolv@gmail.com>
2023-04-27 17:28:44 -0400

    navbar and routes set up

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import NavBar from './components/NavBar';
 
-
 function App() {
-  return (
-    <div>
-      <NavBar />
-    </div>
-  );
-}
 
-export default App;
+  
+   return (
+     <div>
+       <NavBar/>
+     </div>
+   );
+ }
+ 
+ export default App;
+ 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,35 +2,41 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import {
+  BrowserRouter as Router,
+  Route, 
+  Routes
+} from "react-router-dom";
+import Dogs from '../pages/Dogs';
+import About from '../pages/About';
+import Home from '../pages/Home';
+
+
 
 export default function NavBar() {
   return (
-    <div>
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">Dogs</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+    <Router>
+      <div>
+        <Navbar bg="dark" variant={"dark"} expand="lg">
+          <Container>
+            <Navbar.Brand href="/">Home</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="dogs">Dogs</Nav.Link>
+                <Nav.Link href="about">About</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+  
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/dogs" element={<Dogs/>}/>
+          <Route exact path="/about" element={<About/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
