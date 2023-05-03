ba9e55b8b65af94f239f4cbe0f5f7f6b6151ad21
LatinainTech <111721203+LatinainTech@users.noreply.github.com>
2023-05-02 20:59:54 -0400

    images uploaded

---
+++ src/IMAGES/images.js
@@ -0,0 +1,10 @@
+
+const IMAGES = {
+    about: require('./Imagess/about.jpeg'),
+    FB: require('./Imagess/FB.jpeg'),
+    GR: require('./Imagess/GR.jpeg'),
+    GS: require('./Imagess/GS.jpeg'),
+    LR: require('./Imagess/LR.jpeg'),
+    P: require('./Imagess/P.jpeg'),
+}
+export default IMAGES;
