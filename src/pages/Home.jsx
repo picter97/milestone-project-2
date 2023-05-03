94d92e9adfff00fd23d8d584075589cd8b3463b7
LatinainTech <111721203+LatinainTech@users.noreply.github.com>
2023-05-02 22:49:32 -0400

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,38 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>this is Home</div>
+    <div>Home
+
+    <body>
+      <header>
+        <h1> All About Dogs</h1>
+        <p>Welcome to our Homepage! Here you will find information about some of the most popular dog breeds in the world. </p>
+      </header>
+
+    <nav>
+      <a href="#french-bulldog">French Bulldogs</a>
+      <a href="#labrador-retriever">Labrador Retrievers</a>
+      <a href="#Golden-retriever">Golden Retrievers</a>
+      <a href="#German-Shepherds"> Shepherds Retrievers</a>
+      <a href="#poodle">Poodles</a>
+    </nav>
+
+    <section id="french-bulldog">
+      <h2>French Bulldogs</h2>
+  
+    </section>
+
+
+
+
+
+
+    </body>
+      
+      
+      
+      
+      
+      </div>
   )
 }
