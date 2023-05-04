864a6cccc570b87d43ed4598cdc172e519304f79
LatinainTech <111721203+LatinainTech@users.noreply.github.com>
2023-05-04 14:13:18 -0400

    added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
   return (
     <div> 
-    
+      <img alt='./IMAGES/imagess/about.jpeg'></img>
       <p>
       Welcome to our about page where we'll introduce you to some of the most beloved dog breeds: the French Bulldog, Labrador Retriever, Golden Retriever, German Shepherd, and Poodle. 
       Each of these breeds has its unique characteristics, ranging from their size, lifespan, temperament, and overall personality. 
@@ -11,9 +11,7 @@ export default function About() {
       We hope you enjoy learning about these breeds and find the information here useful in making an informed decision when it comes to choosing a dog that's perfect for you and your family.
       </p>
 
-
       <p> DOGS LIVE TO PLAY, NOW LET THEM PLAY TO LIVE </p>
      </div>
-  /// add a video ///
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,45 @@
 import React from 'react'
 
-export default function Home() {
+ export default function Home() {
   return (
-    <div>Home
-
-    <body>
-      <header>
-        <h1> All About Dogs</h1>
-        <p>Welcome to our Homepage! Here you will find information about some of the most popular dog breeds in the world. </p>
+    <div>
+      <header style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '20px', marginBottom: '20px' }}>
+      
+        <h1>WELCOME ! </h1>
+        <p>Here you will find information about different dog breeds, key facts, and other resources for dog lovers.</p>
       </header>
+   <section>
+          <h2 className="SubTitle">Meet Our Furry Friends</h2>
+          <p className="Description">We love all dog breeds and have plenty of lovable pups. Our mission is to teach you important facts and knowledge so that you will be prepared when you get your first dog ! </p>
+          <div className="Facts">
+              <h3 className="FactTitle">Over 50 Breeds</h3>
+              <p className="FactDescription">From adorable French Bulldogs to energetic Labrador Retrievers, we have a variety of breeds.</p>   
+        </div>   
+        <div className="ContactSection">
+        <div className="ContactContainer">
+          <h2 className="SubTitle">Contact Us</h2>
+          <form className="ContactForm">
+          <div className="FormFields">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" placeholder="Enter your name" />
+          </div>
+          <div className="FormFields">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" placeholder="Enter your email address" />
+          </div>
+          <div className="FormFields">
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" placeholder="Enter your message"></textarea>
+          </div> 
+            <button type="submit">Send</button>
+          </form>
+        </div>
+    <div>
+      <img alt=" "></img>
 
-    <nav>
-      <a href="#french-bulldog">French Bulldogs</a>
-      <a href="#labrador-retriever">Labrador Retrievers</a>
-      <a href="#Golden-retriever">Golden Retrievers</a>
-      <a href="#German-Shepherds"> Shepherds Retrievers</a>
-      <a href="#poodle">Poodles</a>
-    </nav>
-
-    <section id="french-bulldog">
-      <h2>French Bulldogs</h2>
-  
-    </section>
-
-
-
-
-
-
-    </body>
-      
-      
-      
-      
-      
-      </div>
-  )
-}
+    </div>
+    </div>
+   </section>
+</div>
+  );
+ }
