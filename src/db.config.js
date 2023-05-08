76435fd0c84534d295038fb0c41ba924cfaba892
Resnick1022 <114195739+Resnick1022@users.noreply.github.com>
2023-05-07 20:02:17 -0400

    Create db.config.js

---
+++ src/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: process.env.commenttest2.cgub1iw4kin1.us-east-1.rds.amazonaws.com,
+  USER: process.env.Resnick1022,
+  PASSWORD: process.env.postgres,
+  DB: process.env.commenttest2,
+  dialect: "postgres",
