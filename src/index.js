40735120514dd7cd9896b3dae7ba1e63249c8019
Resnick1022 <114195739+Resnick1022@users.noreply.github.com>
2023-05-07 20:08:11 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,6 +12,23 @@ root.render(
   </React.StrictMode>
 );
 
+const dbConfig = require("../db.config.js");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.commenttest2, dbConfig.Resnick1022, dbConfig.postgres, {
+  host: dbConfig.env.commenttest2.cgub1iw4kin1.us-east-1.rds.amazonaws.com
+  ,
+  dialect: dbConfig.dialect,
+  });
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.blog = require("./blog.model.js")(sequelize, Sequelize);
+
+module.exports = db;
+  
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
